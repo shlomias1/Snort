ab9b9300ab804741e489bd4a9d22465cca97e940
Shlomi Assayag <112924575+shlomias1@users.noreply.github.com>
2025-02-26 07:57:30 +0200

    Update config.py

--- config.py
+++ config.py
@@ -6,13 +6,19 @@ LEARNING_RATE = 0.001
 BATCH_SIZE = 16
 EPOCHS = 50
 HIDDEN_LAYER_SIZE = 256
-FEATURE_VECTOR_SIZE = 2421
+FEATURE_VECTOR_SIZE = 400
 # Parameters for Genroot Games
 NUM_GAMES = 10000
 # Parameters for MCTS and PUCT
-MCTS_SIMULATIONS = 1000
-PUCT_SIMULATIONS = 1000
+INFO_RETENTION_TIME = 5
+MCTS_SIMULATIONS = 700
+PUCT_SIMULATIONS = 700
 EXPLORATION_WEIGHT = 1
+RESULT_MAP = {
+    "Winner: R": 1,    # Red Victory → 1
+    "Winner: B": -1  # Blue Victory → -1
+}
+CPUCT = 1.0
 #dirs
 DATA_DIR = "data"
-LOG_DIR = "logs"
+LOG_DIR = "snort_logs"
