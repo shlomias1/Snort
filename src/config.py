76554911c811d53fe876813041a8db2667d606be
Shlomi Assayag <112924575+shlomias1@users.noreply.github.com>
2025-03-12 08:31:53 +0200

    Update config.py
    Constant changes to improve game quality

--- src/config.py
+++ src/config.py
@@ -1,23 +1,25 @@
 # Game board settings
-SIZE = 10
+SIZE = 8
+NUM_CHANNELS = 6
 NUM_BLOKED_CELLS = 3
 # Neural network training parameters
 LEARNING_RATE = 0.001
 BATCH_SIZE = 16
 EPOCHS = 50
 HIDDEN_LAYER_SIZE = 256
-FEATURE_VECTOR_SIZE = 400
+FEATURE_VECTOR_SIZE = SIZE*SIZE*NUM_CHANNELS # (8, 8, 6) = 384
 # Parameters for Genroot Games
 NUM_GAMES = 10000
 NUM_GAMES_FOR_PUCT = 500
 # Parameters for MCTS and PUCT
 INFO_RETENTION_TIME = 5
-MCTS_SIMULATIONS = 700
-PUCT_SIMULATIONS = 700
+MCTS_SIMULATIONS = 2000
+PUCT_SIMULATIONS = 2000
 EXPLORATION_WEIGHT = 1
 RESULT_MAP = {
     "Winner: R": 1,    # Red Victory → 1
-    "Winner: B": -1  # Blue Victory → -1
+    "Winner: B": -1,  # Blue Victory → -1
+    "ongoing" : 0
 }
 CPUCT = 0.5
 STARTING_ELO = 1200
